8e877cf2065c6e75555d930eabf9c3a81e768275
Fiddy <85234199+Felixx2112@users.noreply.github.com>
2022-11-09 20:35:11 +0700

    design cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -8,7 +8,7 @@ const Wrapper = styled.div`
   padding: 20px;
   border-top: 0.5px solid #ced6e0;
   display: flex;
-  align-items: center;
+  // align-items: center;
   justify-content: space-between;
 `;
 const Title = styled.h1`
@@ -29,56 +29,149 @@ const CartItem = styled.div`
 `;
 
 const Img = styled.img`
-  width: 320px;
-  height: 320px;
+  width: 160px;
+  height: 160px;
 `;
 
 const CartInfo = styled.div`
   display: flex;
   flex-direction: column;
+  margin-left: 10px;
 `;
 
-const CartTitle = styled.div``;
+const CartTitle = styled.h3`
+  font-weight: 400;
+  color: #2f3542;
+`;
 
-const CartDesc = styled.div``;
+const CartDesc = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  gap: 5px;
+  color: #57606f;
+`;
 
-const CartProduct = styled.div``;
+const CartProduct = styled.span`
+  font-size: 15px;
+  font-weight: 400;
+`;
 
-const CartColor = styled.div``;
+const CartColor = styled.span`
+  font-size: 15px;
+  font-weight: 400;
+`;
 
-const CartSize = styled.div``;
+const CartSize = styled.span`
+  font-size: 15px;
+  font-weight: 400;
+`;
 
-const CartPrice = styled.span``;
+const CartPrice = styled.span`
+  font-size: 20px;
+  font-weight: 500;
+  color: #2f3542;
+`;
+const Button = styled.button`
+  margin-top: 5px;
+  padding: 5px;
+  width: 30%;
+  outline: none;
+  border-radius: 5px;
+  border: 0.5px solid #ff6b81;
+  color: #ff6b81;
+  background-color: #fff;
+  &:hover {
+    color: #fff;
+    background-color: #ff6b81;
+  }
+`;
 
-const CartAmount = styled.div``;
+const CartAmount = styled.div`
+  margin-right: 30px;
+`;
 
-const Filter = styled.div``;
+const FilterText = styled.div`
+  font-size: 15px;
+  font-weight: 400;
+  color: #57606f;
+`;
 
-const FilterText = styled.div``;
-
-const Select = styled.select``;
+const Select = styled.select`
+  padding: 5px 6px;
+  font-weight: 400;
+  font-size: 14px;
+  margin-left: 5px;
+  margin-bottom: 10px;
+  outline: none;
+`;
 
 const Option = styled.option``;
 
-const AmountProduct = styled.div``;
+const AmountProduct = styled.span`
+  font-size: 15px;
+  font-weight: 400;
+  color: #57606f;
+`;
 
-const Amount = styled.div``;
-
-const AmountTitle = styled.div``;
-
-const AmountContainer = styled.div``;
-
-const Shipping = styled.div``;
+const Amount = styled.span`
+  font-weight: bold;
+  color: #2f3542;
+`;
 
 const TotalAmount = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Subtotal = styled.div``;
+const Total = styled.div`
+  padding: 20px;
+  background-color: #f1f2f6;
+  border-radius: 5px;
+`;
+const TitleTotal = styled.div``;
+const TitleAmount = styled.h3`
+  padding-bottom: 20px;
+  border-bottom: 0.5px solid #a4b0be;
+`;
 
-const EstimatedTax = styled.div``;
+const TitlePrice = styled.h3`
+  padding-top: 20px;
+  border-top: 0.5px solid #a4b0be;
+`;
 
-const OrderTotal = styled.div``;
+const ItemAmount = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin: 20px 0px;
+`;
+const Item = styled.span`
+  font-size: 15px;
+  font-weight: 400;
+  color: #57606f;
+`;
+
+const Desc = styled.p`
+  margin: 20px;
+  font-weight: 400;
+  color: #2f3542;
+`;
+const ButtonAmount = styled.button`
+  padding: 15px;
+  // margin: auto;
+  outline: none;
+  color: #ff6b81;
+  border-radius: 5px;
+  border: 0.5px solid #ff6b81;
+  background-color: #fff;
+  cursor: pointer;
+  &:hover {
+    background-color: #ff6b81;
+    color: #fff;
+  }
+`;
 
 function Cart() {
   return (
@@ -92,45 +185,127 @@ function Cart() {
             <CartInfo>
               <CartTitle>Wool Cashmere Chesterfield Coat</CartTitle>
               <CartDesc>
-                <CartProduct>449890</CartProduct>
-                <CartColor>69 navay</CartColor>
-                <CartSize>Unisex M</CartSize>
+                <CartProduct>Product ID: 449890</CartProduct>
+                <CartColor>Color: 69 navay</CartColor>
+                <CartSize>Size: Unisex M</CartSize>
                 <CartPrice>$199.90</CartPrice>
               </CartDesc>
+              <Button>Remove</Button>
             </CartInfo>
           </CartItem>
           <CartAmount>
-            <Filter>
-              <FilterText>
-                Quantity
-                <Select>
-                  <Option>1</Option>
-                  <Option>2</Option>
-                  <Option>3</Option>
-                  <Option>4</Option>
-                  <Option>5</Option>
-                  <Option>6</Option>
-                  <Option>7</Option>
-                  <Option>8</Option>
-                  <Option>9</Option>
-                  <Option>10</Option>
-                </Select>
-              </FilterText>
-            </Filter>
+            <FilterText>
+              Quantity
+              <Select>
+                <Option>1</Option>
+                <Option>2</Option>
+                <Option>3</Option>
+                <Option>4</Option>
+                <Option>5</Option>
+                <Option>6</Option>
+                <Option>7</Option>
+                <Option>8</Option>
+                <Option>9</Option>
+                <Option>10</Option>
+              </Select>
+            </FilterText>
+
             <AmountProduct>
-              Subtotal:
-              <Amount>$222</Amount>
+              Subtotal:<Amount>$199.90</Amount>
+            </AmountProduct>
+          </CartAmount>
+        </CartList>
+
+        <CartList>
+          <CartItem>
+            <Img src="https://image.uniqlo.com/UQ/ST3/us/imagesgoods/449890/item/usgoods_69_449890.jpg?width=320" />
+            <CartInfo>
+              <CartTitle>Wool Cashmere Chesterfield Coat</CartTitle>
+              <CartDesc>
+                <CartProduct>Product ID: 449890</CartProduct>
+                <CartColor>Color: 69 navay</CartColor>
+                <CartSize>Size: Unisex M</CartSize>
+                <CartPrice>$199.90</CartPrice>
+              </CartDesc>
+              <Button>Remove</Button>
+            </CartInfo>
+          </CartItem>
+          <CartAmount>
+            <FilterText>
+              Quantity
+              <Select>
+                <Option>1</Option>
+                <Option>2</Option>
+                <Option>3</Option>
+                <Option>4</Option>
+                <Option>5</Option>
+                <Option>6</Option>
+                <Option>7</Option>
+                <Option>8</Option>
+                <Option>9</Option>
+                <Option>10</Option>
+              </Select>
+            </FilterText>
+
+            <AmountProduct>
+              Subtotal:<Amount>$199.90</Amount>
+            </AmountProduct>
+          </CartAmount>
+        </CartList>
+
+        <CartList>
+          <CartItem>
+            <Img src="https://image.uniqlo.com/UQ/ST3/us/imagesgoods/449890/item/usgoods_69_449890.jpg?width=320" />
+            <CartInfo>
+              <CartTitle>Wool Cashmere Chesterfield Coat</CartTitle>
+              <CartDesc>
+                <CartProduct>Product ID: 449890</CartProduct>
+                <CartColor>Color: 69 navay</CartColor>
+                <CartSize>Size: Unisex M</CartSize>
+                <CartPrice>$199.90</CartPrice>
+              </CartDesc>
+              <Button>Remove</Button>
+            </CartInfo>
+          </CartItem>
+          <CartAmount>
+            <FilterText>
+              Quantity
+              <Select>
+                <Option>1</Option>
+                <Option>2</Option>
+                <Option>3</Option>
+                <Option>4</Option>
+                <Option>5</Option>
+                <Option>6</Option>
+                <Option>7</Option>
+                <Option>8</Option>
+                <Option>9</Option>
+                <Option>10</Option>
+              </Select>
+            </FilterText>
+
+            <AmountProduct>
+              Subtotal:<Amount>$199.90</Amount>
             </AmountProduct>
           </CartAmount>
         </CartList>
         <TotalAmount>
-          <AmountTitle>Order summary 4 Item(s)</AmountTitle>
-          <AmountContainer>
-            <Shipping>GHN</Shipping>
-            <Subtotal>$453</Subtotal>
-            <EstimatedTax>GHN</EstimatedTax>
-          </AmountContainer>
-          <OrderTotal>$453</OrderTotal>
+          <Total>
+            <TitleTotal>
+              <TitleAmount>Order summary</TitleAmount>
+              <ItemAmount>
+                <Item>Item(s) subtotal</Item>
+                <Item>Item(s) subtotal</Item>
+                <Item>Item(s) subtotal</Item>
+              </ItemAmount>
+              <TitlePrice>Order summary</TitlePrice>
+            </TitleTotal>
+          </Total>
+          <Desc>
+            Your order qualifies for free standard shipping. Click "Checkout" to
+            reserve Cart contents for 60 minutes.
+          </Desc>
+          <ButtonAmount>Checkout</ButtonAmount>
         </TotalAmount>
       </Wrapper>
       <Footer />
